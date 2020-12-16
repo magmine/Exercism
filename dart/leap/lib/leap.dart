953585846169d9d82592ef1559f0bca68f20dbfc
magmine <aminemag96@gmail.com>
2020-12-16 11:25:12 +0100

    one liner

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,15 +1,5 @@
 class Leap {
   bool leapYear([int year]) {
-    if (year % 4 == 0) {
-      if (year % 100 == 0) {
-        if (year % 400 == 0) {
-          return true;
-        }
-        return false;
-      } else {
-        return true;
-      }
-    }
-    return false;
+    return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
   }
 }

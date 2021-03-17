a178b41b276c3a83f00dcb4b5e667cdb912a28f5
magmine <aminemag96@gmail.com>
2021-03-17 12:19:34 +0100

    update for more readability

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,32 +1,27 @@
 class Matrix:
-    def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
+    def __init__(self, matrix_string : str):
         self.rows = []
         self.cols = []
         rows_string = matrix_string.split("\n")
-        self.rows = self.get_rows_from_string(rows_string)
-        self.cols = self.get_columns_from_rows()
+        self.get_rows_from_string(rows_string)
+        self.get_columns_from_rows()
 
 
 
-    def row(self, index):
+    def row(self, index : int) -> []:
         return self.rows[index - 1]
 
-    def column(self, index):
+    def column(self, index : int) -> []:
         return self.cols[index - 1]
 
-    def get_rows_from_string(self, rows_string : str) -> []:
-        rows = []
+    def get_rows_from_string(self, rows_string : str):
         for row_string in rows_string:
             row_string = row_string.strip()
             row_string = row_string.split(" ")
-            rows.append([int(num) for num in row_string])
-        return rows
+            self.rows.append([int(num) for num in row_string])
 
     def get_columns_from_rows(self):
-        cols = []
         if len(self.rows) == 0:
-            return cols
+            return
         for i in range(len(self.rows[0])):
-            cols.append([row[i] for row in self.rows])
-        return cols
+            self.cols.append([row[i] for row in self.rows])

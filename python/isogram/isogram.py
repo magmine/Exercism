2c467e1da8d1d87ac103a9c19c6988e3db20200b
magmine <aminemag96@gmail.com>
2021-03-17 16:15:55 +0100

    isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,5 @@
 import re
 
 def is_isogram(string : str):
-    string = re.sub("[^A-Za-z]", "", string).lower()
-    for i in range(len(string)):
-        if string.count(string[i]) > 1:
-            return False
-    return True
+    s = [ch for ch in string.lower() if ch.isalpha()]
+    return len(set(s)) == len(s)

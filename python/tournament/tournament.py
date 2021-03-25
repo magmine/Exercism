8ecf9643f7ccc4aabf5aa7cdba33934d5c9febe5
magmine <aminemag96@gmail.com>
2021-03-25 18:38:55 +0100

    Completed, learned sorting using custom comparators in python

--- python/tournament/tournament.py
+++ python/tournament/tournament.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 def tally(rows):
     matches = get_matches(rows)
     data = {}
@@ -14,15 +16,18 @@ def tally(rows):
             result_team1 = [1, 0, 0, 1, 0]
         else:
             result_team1 = [1, 0, 1, 0, 1]
-            result_team2 = result_team1
+            result_team2 = [1, 0, 1, 0, 1]
         
         if len(data.get(match[0], [])) == 0:
             data.setdefault(match[0], result_team1)
-            data.setdefault(match[1], result_team2)
         else:
             team1_current_result = data[match[0]]
-            team2_current_result = data[match[1]]
             data[match[0]] = add_match_result(team1_current_result, result_team1)
+        
+        if len(data.get(match[1], [])) == 0:
+            data.setdefault(match[1], result_team2)
+        else:
+            team2_current_result = data[match[1]]
             data[match[1]] = add_match_result(team2_current_result, result_team2)
 
     sorted_res = sort_dict(data)
@@ -41,15 +46,27 @@ def add_match_result(current_resutls: [], match_results: []) -> []:
         current_resutls[i] += match_results[i]
     return current_resutls
 
+def compare_func(x, y):
+    if x[1][4] < y[1][4]:
+        return 1
+    elif x[1][4] > y[1][4]:
+        return -1
+
+    if x[0] < y[0]:
+        return -1
+    else:
+        return 1
+
 def sort_dict(dict_to_sort: {}):
-    return dict(sorted(dict_to_sort, key=lambda x: (x[1][0], x[0])))
+    # step_1 = dict(sorted(dict_to_sort.items(), key=lambda x: (x[1][4], x[0]), reverse=True))
+    # return dict(sorted(step_1.items(), key=lambda x: (x[1][4], x[0])))
+    return dict(sorted(dict_to_sort.items(), key=cmp_to_key(compare_func)))
 
 def create_result_table(dict: {}) -> []:
     result_table = []
-    results_table_format = '{:31s}|{:^ 4d}|{:^ 4d}|{:^ 4d}|{:^ 4d}|{:>4d}'
-    header_table_format = '{:31s}|{:^4s}|{:^4s}|{:^4s}|{:^4s}|{:>4s}'
-    result_table.append(header_table_format.format("Team", "MP", "W", "D", "L", "P"))
+    results_table_format = '{:31s}|{:^ 4d}|{:^ 4d}|{:^ 4d}|{:^ 4d}|{:>3d}'
+    result_table.append("Team                           | MP |  W |  D |  L |  P")
     for team, results in dict.items():
         result_table.append(results_table_format.format(team, *results))
-    
+    print(result_table)
     return result_table
--- python/tournament/tournament_test.py
+++ python/tournament/tournament_test.py
@@ -6,7 +6,7 @@ from tournament import tally
 
 
 class TournamentTest(unittest.TestCase):
-    def xtest_just_the_header_if_no_input(self):
+    def test_just_the_header_if_no_input(self):
         results = []
         table = ["Team                           | MP |  W |  D |  L |  P"]
         self.assertEqual(tally(results), table)
@@ -20,7 +20,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_a_win_can_also_be_expressed_as_a_loss(self):
+    def test_a_win_can_also_be_expressed_as_a_loss(self):
         results = ["Blithering Badgers;Allegoric Alaskans;loss"]
         table = [
             "Team                           | MP |  W |  D |  L |  P",
@@ -29,7 +29,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_a_different_team_can_win(self):
+    def test_a_different_team_can_win(self):
         results = ["Blithering Badgers;Allegoric Alaskans;win"]
         table = [
             "Team                           | MP |  W |  D |  L |  P",
@@ -38,7 +38,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_a_draw_is_one_point_each(self):
+    def test_a_draw_is_one_point_each(self):
         results = ["Allegoric Alaskans;Blithering Badgers;draw"]
         table = [
             "Team                           | MP |  W |  D |  L |  P",
@@ -47,7 +47,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_there_can_be_more_than_one_match(self):
+    def test_there_can_be_more_than_one_match(self):
         results = [
             "Allegoric Alaskans;Blithering Badgers;win",
             "Allegoric Alaskans;Blithering Badgers;win",
@@ -59,7 +59,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_there_can_be_more_than_one_winner(self):
+    def test_there_can_be_more_than_one_winner(self):
         results = [
             "Allegoric Alaskans;Blithering Badgers;loss",
             "Allegoric Alaskans;Blithering Badgers;win",
@@ -71,7 +71,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_there_can_be_more_than_two_teams(self):
+    def test_there_can_be_more_than_two_teams(self):
         results = [
             "Allegoric Alaskans;Blithering Badgers;win",
             "Blithering Badgers;Courageous Californians;win",
@@ -85,7 +85,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_typical_input(self):
+    def test_typical_input(self):
         results = [
             "Allegoric Alaskans;Blithering Badgers;win",
             "Devastating Donkeys;Courageous Californians;draw",
@@ -103,7 +103,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_incomplete_competition_not_all_pairs_have_played(self):
+    def test_incomplete_competition_not_all_pairs_have_played(self):
         results = [
             "Allegoric Alaskans;Blithering Badgers;loss",
             "Devastating Donkeys;Allegoric Alaskans;loss",
@@ -119,7 +119,7 @@ class TournamentTest(unittest.TestCase):
         ]
         self.assertEqual(tally(results), table)
 
-    def xtest_ties_broken_alphabetically(self):
+    def test_ties_broken_alphabetically(self):
         results = [
             "Courageous Californians;Devastating Donkeys;win",
             "Allegoric Alaskans;Blithering Badgers;win",

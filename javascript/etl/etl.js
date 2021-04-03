2be7b755e1d17bb85e47f50c2049feb1edbb7166
magmine <aminemag96@gmail.com>
2021-04-03 19:23:32 +0100

    etl

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -4,39 +4,21 @@
 //
 
 export const transform = (old) => {
-  for (let key of old) {
-    chars = old[key]
-    chars.map(())
+  const newT = {};
+
+  for (let key in old) {
+    const chars = old[key];
+    chars.map((value) => {
+      newT[value] = key;
+    })
   }
-  old.map((val, i) => {
-    console.log(val, i)
-  });
+
+  const keys = Object.keys(newT).sort();
+
+  const result = {};
+
+  for (let key of keys) {
+    result[key.toLowerCase()] = Number(newT[key]);
+  }
+  return result;
 };
-
-
-const old = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z'],
-};
-
-const newT = {}
-
-for (let key in old) {
-  chars = old[key]
-  chars.map((value) => {
-    newT[value] = key
-  })
-}
-
-keys = Object.keys(newT).sort()
-
-const result = {}
-
-for (let key of )
-
-console.log(newT)
